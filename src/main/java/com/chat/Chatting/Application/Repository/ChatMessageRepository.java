8ecc8b3dcaba494340a326e883a847fa03c093be
Kalyani <boorakalyani20@gmail.com>
2025-03-10 20:50:37 +0530

    Add JPA repository for ChatMessageRepository to manage CRUD operations

--- src/main/java/com/chat/Chatting/Application/Repository/ChatMessageRepository.java
+++ src/main/java/com/chat/Chatting/Application/Repository/ChatMessageRepository.java
@@ -1,1 +1,10 @@
  
+package com.chat.Chatting.Application.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chat.Chatting.Application.Entity.ChatDtoMessages;
+
+public interface ChatMessageRepository  extends JpaRepository<ChatDtoMessages, Long>  {
+
+}
