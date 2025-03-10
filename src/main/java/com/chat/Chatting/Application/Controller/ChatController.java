c548eb7bca6f9325dc0fc1de98e2c218828661a8
Kalyani <boorakalyani20@gmail.com>
2025-03-10 20:34:01 +0530

    Add message handling for chat application with WebSocket

--- src/main/java/com/chat/Chatting/Application/Controller/ChatController.java
+++ src/main/java/com/chat/Chatting/Application/Controller/ChatController.java
@@ -1,1 +1,66 @@
+package com.chat.Chatting.Application.Controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.chat.Chatting.Application.Entity.ChatDtoMessages;
+import com.chat.Chatting.Application.Repository.ChatMessageRepository;
+import com.chat.Chatting.Application.Repository.MessageType;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+public class ChatController {
+	    private final ChatMessageRepository chatMessageRepository;
+	    public ChatController(ChatMessageRepository chatMessageRepository) {
+	    	this.chatMessageRepository = chatMessageRepository;
+	    }
+
+	    @MessageMapping("/chat.sendMessage")
+	    @SendTo("/topic/public")
+	    public ChatDtoMessages sendMessage(@Payload ChatDtoMessages msg) {
+	        // Save to database
+	        ChatDtoMessages entity =  ChatDtoMessages.builder()
+	                .type(msg.getType())
+	                .content(msg.getContent())
+	                .sender(msg.getSender())
+	                .build();
+	        chatMessageRepository.save(entity);
+
+	        return msg;
+	    }
+
+/*
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+public ChatDtoMessages sendMsg(@Payload ChatDtoMessages msg) {
+		return msg;
+}*/
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+public ChatDtoMessages addUser(@Payload ChatDtoMessages msg,SimpMessageHeaderAccessor headerAccessor) {
+		// Add username in web socket session.
+		headerAccessor.getSessionAttributes().put("username", msg.getSender());
+		return msg;
+	}
+	
+	@MessageMapping("/chat.leave")
+	@SendTo("/topic/public")
+	public ChatDtoMessages leaveChat(@Payload ChatDtoMessages msg, SimpMessageHeaderAccessor headerAccessor) {
+	    headerAccessor.getSessionAttributes().remove("username"); // Remove user from session
+
+	    return ChatDtoMessages.builder()
+	            .type(MessageType.LEAVE)
+	            .sender(msg.getSender())
+	            .build();
+	}
+}
+
+
+
  
